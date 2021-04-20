d0d696244ed90f95189d6bcb5b8a73edc4307596
Kaio <kaio.moraes@zup.com.br>
2021-04-20 13:43:52 -0300

    Criar classe de ContatoRepository.

---
+++ src/main/java/br/com/zup/Marketing/repository/ContatoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.Marketing.repository;
+
+import br.com.zup.Marketing.model.Contato;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContatoRepository extends CrudRepository<Contato, String> {
+}
