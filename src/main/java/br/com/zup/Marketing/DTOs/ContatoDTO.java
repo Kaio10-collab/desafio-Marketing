ecf6c4798bd0f747da63bce059054d7e70ad57d2
Kaio <kaio.moraes@zup.com.br>
2021-04-22 13:55:15 -0300

    Criar método de conversão DTO para Model na classe ContatoDTO.

--- src/main/java/br/com/zup/Marketing/DTOs/ContatoDTO.java
+++ src/main/java/br/com/zup/Marketing/DTOs/ContatoDTO.java
@@ -1,4 +1,50 @@
 package br.com.zup.Marketing.DTOs;
 
+import br.com.zup.Marketing.model.Contato;
+
+import javax.validation.constraints.NotBlank;
+
 public class ContatoDTO {
+
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private double telefone;
+
+    public ContatoDTO() {
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(double telefone) {
+        this.telefone = telefone;
+    }
+
+    public Contato converterDTOParaModel(){
+        Contato contato = new Contato();
+        contato.setNomeCompleto(this.nomeCompleto);
+        contato.setEmail(this.email);
+        contato.setTelefone(this.telefone);
+        return contato;
+    }
 }
