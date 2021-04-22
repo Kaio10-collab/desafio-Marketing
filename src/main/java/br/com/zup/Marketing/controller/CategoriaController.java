9e082153f8c03284af635c73951859757a099fc9
Kaio <kaio.moraes@zup.com.br>
2021-04-22 11:28:39 -0300

    Criar método de cadastrar e deletar das classes controller e service da entidade categoria.

--- src/main/java/br/com/zup/Marketing/controller/CategoriaController.java
+++ src/main/java/br/com/zup/Marketing/controller/CategoriaController.java
@@ -1,13 +1,27 @@
 package br.com.zup.Marketing.controller;
 
+import br.com.zup.Marketing.model.Categoria;
 import br.com.zup.Marketing.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("categorias/")
 public class CategoriaController {
 
+    @Autowired
+    private CategoriaService categoriaService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Categoria cadastroCategoria(@RequestBody Categoria categoria) {
+        return categoriaService.cadastrarCategoria(categoria);
+    }
+
+    @DeleteMapping("{id}/")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategoria(@PathVariable Integer id){
+        categoriaService.deletarCategoria(id);
+    }
 }
--- src/main/java/br/com/zup/Marketing/service/CategoriaService.java
+++ src/main/java/br/com/zup/Marketing/service/CategoriaService.java
@@ -1,4 +1,21 @@
 package br.com.zup.Marketing.service;
 
+import br.com.zup.Marketing.model.Categoria;
+import br.com.zup.Marketing.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria cadastrarCategoria(Categoria categoria){
+        return categoriaRepository.save(categoria);
+    }
+
+    public void deletarCategoria(Integer id){
+        categoriaRepository.deleteById(id);
+    }
 }
