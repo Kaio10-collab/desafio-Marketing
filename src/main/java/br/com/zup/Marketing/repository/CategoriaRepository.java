65050f9057f1a2a28a284da104e8af17b96729c5
Kaio <kaio.moraes@zup.com.br>
2021-04-22 09:14:44 -0300

    Criar classes do Repository da categoria e produto.

---
+++ src/main/java/br/com/zup/Marketing/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.Marketing.repository;
+
+import br.com.zup.Marketing.model.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaRepository extends CrudRepository<Categoria, String> {
+}
---
+++ src/main/java/br/com/zup/Marketing/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.Marketing.repository;
+
+import br.com.zup.Marketing.model.Produto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProdutoRepository extends CrudRepository<Produto, String> {
+}
