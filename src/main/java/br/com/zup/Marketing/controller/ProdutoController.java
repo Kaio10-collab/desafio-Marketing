c3266724caa68567487a6a184f007bfd15ab6cf0
Kaio <kaio.moraes@zup.com.br>
2021-04-22 11:50:30 -0300

    Criar métodos de cadastro e delete na controller e service da entidade produto.

--- src/main/java/br/com/zup/Marketing/controller/ProdutoController.java
+++ src/main/java/br/com/zup/Marketing/controller/ProdutoController.java
@@ -1,9 +1,10 @@
 package br.com.zup.Marketing.controller;
 
+import br.com.zup.Marketing.model.Produto;
 import br.com.zup.Marketing.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("produtos/")
@@ -11,4 +12,16 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto cadastroProduto(@RequestBody Produto produto){
+        return produtoService.cadastrarProduto(produto);
+    }
+
+    @DeleteMapping("{id}/")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduto(@PathVariable Integer id){
+        produtoService.deletarProduto(id);
+    }
 }
--- src/main/java/br/com/zup/Marketing/service/ProdutoService.java
+++ src/main/java/br/com/zup/Marketing/service/ProdutoService.java
@@ -1,4 +1,21 @@
 package br.com.zup.Marketing.service;
 
+import br.com.zup.Marketing.model.Produto;
+import br.com.zup.Marketing.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto cadastrarProduto(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    public void deletarProduto(Integer id){
+        produtoRepository.deleteById(id);
+    }
 }
