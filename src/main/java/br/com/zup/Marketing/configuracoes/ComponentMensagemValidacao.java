311c76598b7dd2c03e7bc171fdcb07710036cf8f
Kaio <kaio.moraes@zup.com.br>
2021-04-26 16:42:29 -0300

    Criar classe de configuração de segurança e método CORS.

--- src/main/java/br/com/zup/Marketing/configuracoes/ComponentMensagemValidacao.java
+++ src/main/java/br/com/zup/Marketing/configuracoes/ComponentMensagemValidacao.java
@@ -2,9 +2,17 @@ package br.com.zup.Marketing.configuracoes;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Component // vai transformar a classe em um objeto e vai instaciar com os métodos do Bean.
 // INSTANCIA UM OBJETO Da classe marcada que o spring boot vai gerenciar (bean).
@@ -29,5 +37,43 @@ public class ComponentMensagemValidacao {
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
         bean.setValidationMessageSource(ferramentaDeMensagens());
         return bean;
+    }
+
+    @Configuration // vai criar um objeto de configuração
+    @EnableWebSecurity // habilita os recursos de segurança em nossa aplicação.
+    public static class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+            private static final String[] PUBLIC_MATCHERS_GET = {
+                    "/categorias/**",
+                    "/contatos/**",
+                    "/produtos/**"
+            };
+
+            private static final String[] PUBLIC_MATCHERS_POST = {
+                    "/categorias/**",
+                    "/contatos/**",
+                    "/produtos/**"
+            };
+
+            //Método com mais de um endpoint: http.authorizeRequests().antMatchers(HttpMethod.GET, "/bandas/**").permitAll()
+    //                .antMatchers(HttpMethod.POST).permitAll().antMatchers(HttpMethod.PUT, "/bandas/").permitAll()
+    //                .anyRequest().authenticated();
+
+            @Override // digite conf que irá aparecer as opções prontas; HttpSecurity http é um obj de segurança.
+            protected void configure(HttpSecurity http) throws Exception {
+                http.csrf().disable();
+                http.cors();
+
+                http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                        .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                        .anyRequest().authenticated();
+            }
+
+            @Bean
+            CorsConfigurationSource configuracaoDeCors(){
+                final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+                return source;
+            }
     }
 }
