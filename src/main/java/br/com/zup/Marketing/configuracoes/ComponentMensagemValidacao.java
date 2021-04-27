564c5107f81251da2be5f1b0194790911456670b
Kaio <kaio.moraes@zup.com.br>
2021-04-27 08:53:00 -0300

    Criar classe ConfiguracaoDeSeguranca.

--- src/main/java/br/com/zup/Marketing/configuracoes/ComponentMensagemValidacao.java
+++ src/main/java/br/com/zup/Marketing/configuracoes/ComponentMensagemValidacao.java
@@ -38,42 +38,4 @@ public class ComponentMensagemValidacao {
         bean.setValidationMessageSource(ferramentaDeMensagens());
         return bean;
     }
-
-    @Configuration // vai criar um objeto de configuração
-    @EnableWebSecurity // habilita os recursos de segurança em nossa aplicação.
-    public static class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
-
-            private static final String[] PUBLIC_MATCHERS_GET = {
-                    "/categorias/**",
-                    "/contatos/**",
-                    "/produtos/**"
-            };
-
-            private static final String[] PUBLIC_MATCHERS_POST = {
-                    "/categorias/**",
-                    "/contatos/**",
-                    "/produtos/**"
-            };
-
-            //Método com mais de um endpoint: http.authorizeRequests().antMatchers(HttpMethod.GET, "/bandas/**").permitAll()
-    //                .antMatchers(HttpMethod.POST).permitAll().antMatchers(HttpMethod.PUT, "/bandas/").permitAll()
-    //                .anyRequest().authenticated();
-
-            @Override // digite conf que irá aparecer as opções prontas; HttpSecurity http é um obj de segurança.
-            protected void configure(HttpSecurity http) throws Exception {
-                http.csrf().disable();
-                http.cors();
-
-                http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-                        .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-                        .anyRequest().authenticated();
-            }
-
-            @Bean
-            CorsConfigurationSource configuracaoDeCors(){
-                final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-                return source;
-            }
-    }
 }
