006b3ac76123fc0d387ce4a1c813448051755d4e
Kaio <kaio.moraes@zup.com.br>
2021-04-29 11:19:23 -0300

    Criar atributos, conscructores, gets/sets e métodos implementados.

--- src/main/java/br/com/zup/Marketing/jwt/UsuarioLogin.java
+++ src/main/java/br/com/zup/Marketing/jwt/UsuarioLogin.java
@@ -1,4 +1,80 @@
 package br.com.zup.Marketing.jwt;
 
-public class UsuarioLogin {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+
+public class UsuarioLogin implements UserDetails {
+
+    private String cpf;
+    private String email;
+    private String senha;
+
+    public UsuarioLogin(String cpf, String email, String senha) {
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public UsuarioLogin() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
