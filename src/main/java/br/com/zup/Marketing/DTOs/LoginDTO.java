39d35897ebbe4425192a78205d0284a19d4b7aae
Kaio <kaio.moraes@zup.com.br>
2021-04-29 09:43:32 -0300

    Criar constructor, get/set dos atributos.

--- src/main/java/br/com/zup/Marketing/DTOs/LoginDTO.java
+++ src/main/java/br/com/zup/Marketing/DTOs/LoginDTO.java
@@ -1,4 +1,26 @@
 package br.com.zup.Marketing.DTOs;
 
 public class LoginDTO {
+
+    private String email;
+    private String senha;
+
+    public LoginDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
