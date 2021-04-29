292f0a1e9fdee8b122d3af97c2d67ced4a5563eb
Kaio <kaio.moraes@zup.com.br>
2021-04-29 11:31:13 -0300

    Incluir Optional no ContatoRepository.

--- src/main/java/br/com/zup/Marketing/jwt/UsuarioLoginService.java
+++ src/main/java/br/com/zup/Marketing/jwt/UsuarioLoginService.java
@@ -1,4 +1,20 @@
 package br.com.zup.Marketing.jwt;
 
-public class UsuarioLoginService {
+import br.com.zup.Marketing.repository.ContatoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioLoginService implements UserDetailsService {
+
+    @Autowired
+    private ContatoRepository contatoRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
--- src/main/java/br/com/zup/Marketing/repository/ContatoRepository.java
+++ src/main/java/br/com/zup/Marketing/repository/ContatoRepository.java
@@ -3,8 +3,11 @@ package br.com.zup.Marketing.repository;
 import br.com.zup.Marketing.model.Contato;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ContatoRepository extends CrudRepository<Contato, Integer> {
 
     Iterable<Contato> findByProdutosNome(String nome);
     Iterable<Contato> findByProdutosCategoriasNome(String nome);
+    Optional<Contato> findByEmail(String email);
 }
